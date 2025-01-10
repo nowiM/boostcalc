0fb3ae6e9ec0f0d681f898ffec1ed0def9c0b8c5
KimDoHyeon <ghkdtprhf5@naver.com>
2025-01-10 11:30:09 +0900

    fix: saving-logic-change 브랜치와 merge 후 변경된 변수에 따라 조건 제거

--- src/app/utils/formatting.js
+++ src/app/utils/formatting.js
@@ -3,7 +3,7 @@
 
     // 포커스가 있을 때는 %를 제거하여 숫자만 표시
     export const handleFocus = (e, handleInputChange) => {
-        if (e.target.name === "compoundFrequency" || e.target.name === "investmentPeriod") {
+        if (e.target.name === "compoundFrequency") {
             handleInputChange({
                 target: {
                     name: e.target.name,
@@ -15,7 +15,7 @@
 
     // 포커스가 사라질 때 %를 추가
     export const handleBlur = (e, handleInputChange) => {
-        if (e.target.name === "compoundFrequency" || e.target.name === "investmentPeriod") {
+        if (e.target.name === "compoundFrequency") {
             handleInputChange({
                 target: {
                     name: e.target.name,
